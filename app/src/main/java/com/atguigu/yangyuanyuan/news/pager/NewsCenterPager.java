59c137bcb3a00b1047b0a7dbff7204f34a6aefa4
372991662 <372991662@qq.com>
2016-08-15 21:00:55 +0800

    day3_3联网请求成功

--- app/src/main/java/com/atguigu/yangyuanyuan/news/pager/NewsCenterPager.java
+++ app/src/main/java/com/atguigu/yangyuanyuan/news/pager/NewsCenterPager.java
@@ -2,15 +2,24 @@ package com.atguigu.yangyuanyuan.news.pager;
 
 import android.content.Context;
 import android.graphics.Color;
+import android.util.Log;
 import android.view.Gravity;
 import android.widget.TextView;
 
 import com.atguigu.yangyuanyuan.news.base.BaseViewPager;
+import com.atguigu.yangyuanyuan.news.utils.Constants;
+
+import org.xutils.common.Callback;
+import org.xutils.http.RequestParams;
+import org.xutils.x;
 
 /**
  * Created by 杨媛媛 on 2016/8/15 18:45.
  */
 public class NewsCenterPager extends BaseViewPager {
+
+    private RequestParams params;
+
     public NewsCenterPager(Context context) {
         super(context);
     }
@@ -28,5 +37,38 @@ public class NewsCenterPager extends BaseViewPager {
 
         //添加字数图
         fl_basepager.addView(tv);
+
+        //联网请求数据
+        getDataFromNet();
+    }
+
+    //使用Xutils联网请求数据
+    private void getDataFromNet() {
+        RequestParams params = new RequestParams(Constants.NEWS_CENTER_PAGER_URL);
+        x.http().get(params, new Callback.CommonCallback<String>() {
+
+            @Override
+            public void onSuccess(String result) {
+                Log.e("TAG", "请求成功");
+
+                //设置ListView适配器
+
+            }
+
+            @Override
+            public void onError(Throwable ex, boolean isOnCallback) {
+                Log.e("TAG", "请求错误" + ex);
+            }
+
+            @Override
+            public void onCancelled(CancelledException cex) {
+                Log.e("TAG", "请求取消" + cex);
+            }
+
+            @Override
+            public void onFinished() {
+                Log.e("TAG", "请求结束");
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/atguigu/yangyuanyuan/news/utils/Constants.java
@@ -0,0 +1,10 @@
+package com.atguigu.yangyuanyuan.news.utils;
+
+/**
+ * Created by 杨媛媛 on 2016/8/15 20:41.
+ */
+public class Constants {
+
+    public static final String BASE_URL = "http://192.168.21.41:8080/web_home";
+    public static final String NEWS_CENTER_PAGER_URL = "http://192.168.21.41:8080/web_home/static/api/news/categories.json";
+}
